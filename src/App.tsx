ad8cb1ef69e9b143d36a66a8930f4334e5f1c25e
lensen-degife <lensendegife@gmail.com>
2026-05-23 12:21:04 +0300

    nav bar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
 import JobList from './pages/JobList';
-import PostJob from './pages/PostJob';
+import PostJob from './pages/PostJob';   // assuming you have this
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/jobs" element={<JobList />} />
-          <Route path="/post-job" element={<PostJob />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<JobList />} />
+        <Route path="/post-job" element={<PostJob />} />
+      </Routes>
     </Router>
   );
 }
